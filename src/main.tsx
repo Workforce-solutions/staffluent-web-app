9da1538c7cecc28189cafbb35c90e773e1a71258
Griseld <development@venueboost.io>
2024-12-04 19:47:29 +0200

    feat(staffluent-admin): recent days changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,41 @@
-// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from '@/components/theme-provider'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 import { SessionProvider } from './contexts/SessionContext'
 import router from '@/router/router'
 import '@/index.css'
 import { Provider } from 'react-redux'
 import store from './services/store'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://45748a69284990f4cdf6ecf7651c6b1b@o4508410395230208.ingest.de.sentry.io/4508410905296976",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <SessionProvider>
-        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-          <RouterProvider router={router} />
-          <Toaster />
-        </ThemeProvider>
-      </SessionProvider>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+            <Provider store={store}>
+                <SessionProvider>
+                    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+                        <RouterProvider router={router} />
+                        <Toaster />
+                    </ThemeProvider>
+                </SessionProvider>
+            </Provider>
+        </Sentry.ErrorBoundary>
+    </React.StrictMode>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "vb-all-projects-me",
+    project: "staffluent-platform-web-app"
+  })],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 })
