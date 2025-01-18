46629a7dbd3a5fe31aefcd88000e7cd1d7208a7a
Griseld <development@venueboost.io>
2025-01-18 15:14:51 +0200

    feat: Enhance login page UI and UX

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,41 +1,29 @@
 import { UserAuthForm } from './components/user-auth-form'
-// import ViteLogo from '@/assets/vite.svg'
+import staffLogo from '/images/logo.png'
 
 export default function Login() {
   return (
-    <>
-      <div className='container relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0'>
-        <div className='relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex'>
-          <div className='absolute inset-0 bg-zinc-900' />
-          <div className='relative z-20 flex items-center text-lg font-medium'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='mr-2 h-6 w-6'
-            >
-              <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-            </svg>
-            Staffluent - Platform
-          </div>
+      <div className='min-h-screen bg-gray-50 flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+          <img
+              className='mx-auto h-10 w-auto'
+              src={staffLogo}
+              alt='Staffluent'
+          />
+          <h2 className='mt-6 text-center text-3xl font-semibold text-gray-900'>
+            Welcome to Staffluent
+          </h2>
+            <p className='mt-2 text-center text-sm text-gray-600 max-w-lg mx-auto'>
+                Access your workforce management platform and streamline your team operations,
+                scheduling, and project tracking all in one place
+            </p>
         </div>
-        <div className='lg:p-8'>
-          <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[350px]'>
-            <div className='flex flex-col space-y-2 text-left'>
-              <h1 className='text-2xl font-semibold tracking-tight'>Login</h1>
-              <p className='text-sm text-muted-foreground'>
-                Enter your email and password below <br />
-                to log into your account
-              </p>
-            </div>
+
+        <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+          <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
             <UserAuthForm />
           </div>
         </div>
       </div>
-    </>
   )
 }
