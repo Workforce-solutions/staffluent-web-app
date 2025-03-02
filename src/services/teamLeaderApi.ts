79bf130fcb825211c0f729b1cc4c2cc55ab8d727
gituser <griseldgituser@gmail.com>
2025-03-02 21:05:46 +0500

    Group account by AccountType

--- src/services/teamLeaderApi.ts
+++ src/services/teamLeaderApi.ts
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { getCommonUrl } from '@/hooks/common/common-api-url'
 import {
-    getPrepareHeaders,
-    vbParams,
-    vbUrl,
+  getPrepareHeaders,
+  vbParams,
+  vbUrl,
 } from '@/hooks/common/common-functions'
 import { VenueShortCode } from '@/@types/common'
 import { TeamLeaderResponse } from '@/@types/team-leader'
@@ -12,53 +12,53 @@ import { GetProjectsResponse } from '@/@types/project'
 type IdInterface = { id: number | string } & VenueShortCode
 
 export const teamLeaderApi = createApi({
-    reducerPath: 'teamLeader',
-    baseQuery: fetchBaseQuery({
-        baseUrl: vbUrl + 'vb-apps/staff/employee',
-        prepareHeaders: (headers) => getPrepareHeaders({ headers }),
+  reducerPath: 'teamLeader',
+  baseQuery: fetchBaseQuery({
+    baseUrl: vbUrl + 'vb-apps/staff/employee',
+    prepareHeaders: (headers) => getPrepareHeaders({ headers }),
+  }),
+  tagTypes: ['TeamLeaders', 'Projects'],
+  endpoints: (builder) => ({
+    getQualityInspectionsList: builder.query<TeamLeaderResponse, IdInterface>({
+      query: ({ venue_short_code, id }) => ({
+        url: getCommonUrl({
+          queryString: `/projects/${id}/quality-inspections`,
+          query: `&venue_short_code=${venue_short_code}`,
+          params: vbParams,
+        }),
+      }),
+      providesTags: ['TeamLeaders'],
     }),
-    tagTypes: ['TeamLeaders', 'Projects'],
-    endpoints: (builder) => ({
-        getQualityInspectionsList: builder.query<TeamLeaderResponse, IdInterface>({
-            query: ({ venue_short_code, id }) => ({
-                url: getCommonUrl({
-                    queryString: `/projects/${id}/quality-inspections`,
-                    query: `&venue_short_code=${venue_short_code}`,
-                    params: vbParams,
-                }),
-            }),
-            providesTags: ['TeamLeaders'],
+    createQualityInspection: builder.mutation<
+      TeamLeaderResponse,
+      VenueShortCode & { data: Record<string, string>; id: number }
+    >({
+      query: ({ venue_short_code, data, id }) => ({
+        url: getCommonUrl({
+          queryString: `/projects/${id}/quality-inspections`,
+          params: vbParams,
+          query: `&venue_short_code=${venue_short_code}`,
         }),
-        createQualityInspection: builder.mutation<
-            TeamLeaderResponse,
-            VenueShortCode & { data: Record<string, string>, id: number }
-        >({
-            query: ({ venue_short_code, data, id }) => ({
-                url: getCommonUrl({
-                    queryString: `/projects/${id}/quality-inspections`,
-                    params: vbParams,
-                    query: `&venue_short_code=${venue_short_code}`,
-                }),
-                method: 'POST',
-                body: data,
-            }),
-            invalidatesTags: ['TeamLeaders'],
-        }),
-        getProjectsList: builder.query<GetProjectsResponse, VenueShortCode>({
-            query: ({ venue_short_code }) => ({
-                url: getCommonUrl({
-                    queryString: '/projects',
-                    query: `&venue_short_code=${venue_short_code}`,
-                    params: vbParams,
-                }),
-            }),
-            providesTags: ['Projects'],
-        }),
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['TeamLeaders'],
     }),
+    getProjectsList: builder.query<GetProjectsResponse, VenueShortCode>({
+      query: ({ venue_short_code }) => ({
+        url: getCommonUrl({
+          queryString: '/projects',
+          query: `&venue_short_code=${venue_short_code}`,
+          params: vbParams,
+        }),
+      }),
+      providesTags: ['Projects'],
+    }),
+  }),
 })
 
 export const {
-    useGetQualityInspectionsListQuery,
-    useCreateQualityInspectionMutation,
-    useGetProjectsListQuery,
+  useGetQualityInspectionsListQuery,
+  useCreateQualityInspectionMutation,
+  useGetProjectsListQuery,
 } = teamLeaderApi
